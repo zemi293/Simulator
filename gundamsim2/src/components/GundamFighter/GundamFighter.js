30f37a00a4434b7f8955c0541d445358a2ae4543
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-28 08:54:13 -0700

    Moved files around

---
+++ gundamsim2/src/components/GundamFighter/GundamFighter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM  from 'react';
+import './GundamFighter.css';
+
+class Fighter extends React.Component {
+    constructor(props,GundamName) {
+        super(props)
+        this.state = GundamName //Might not need this
+    }
+
+    render() {
+        return (
+        <div className='GundamFighter'>
+            <div className='GundamImg'>
+                <img src={this.props.GundamList.ShiningG.ImageSrc}></img>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Fighter;
