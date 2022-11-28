5697ceabbb4c7dcc39e7e6b5c2402c780b3e0f93
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-28 08:05:54 -0700

    finished constructor for gundamfighter and hard coded gundam data

--- gundamsim2/src/components/GundamFighter.js
+++ gundamsim2/src/components/GundamFighter.js
@@ -1,2 +1,13 @@
 import React from 'react'
 import ReactDOM  from 'react'
+
+class Fighter extends React.Component {
+    constructor(props,GundamName) {
+        super(props)
+        this.state = GundamName
+    }
+
+    render() {
+
+    }
+}
