21ae429aa7cb8a23c8801e2e1d6a059722ab3078
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-28 09:05:16 -0700

    updated image attributes

--- gundamsim2/src/components/GundamFighter/GundamFighter.js
+++ gundamsim2/src/components/GundamFighter/GundamFighter.js
@@ -12,7 +12,11 @@ class Fighter extends React.Component {
         return (
         <div className='GundamFighter'>
             <div className='GundamImg'>
-                <img src={this.props.GundamList.ShiningG.ImageSrc}></img>
+                <img src={this.props.GundamList.ShiningG.ImageSrc} 
+                height = '500' 
+                width= '350px' 
+                alt = 'GundamFighter'>
+                </img>
             </div>
         </div>
         )
