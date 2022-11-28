623c5327787f0cf78f02b42a4e8cd9d35467ecb3
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-28 08:19:04 -0700

    imported fighter into app.js. created render method for gundam fighter component

--- gundamsim2/src/App.js
+++ gundamsim2/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Fighter from './components/GundamFighter';
 
 const GundamList = { //List of Gundams 
 	StrikeG: {
--- gundamsim2/src/components/GundamFighter.js
+++ gundamsim2/src/components/GundamFighter.js
@@ -4,10 +4,16 @@ import ReactDOM  from 'react'
 class Fighter extends React.Component {
     constructor(props,GundamName) {
         super(props)
-        this.state = GundamName
+        this.state = GundamName //Might not need this
     }
 
     render() {
-
+        return (
+        <div>
+            <div className='GundamImg'></div>
+        </div>
+        )
     }
 }
+
+export default Fighter;
