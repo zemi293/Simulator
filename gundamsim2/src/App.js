385c5fea0babbdf093722cd677d38d966d8021ff
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-28 08:06:28 -0700

    hard coded gundam data

--- gundamsim2/src/App.js
+++ gundamsim2/src/App.js
@@ -1,6 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+const GundamList = { //List of Gundams 
+	StrikeG: {
+		HP: 10,
+		Dex: 3,
+		Str: 3,
+		Const: 4,
+		getStrike: function() {
+			return this.StrikeG
+		}
+	},
+
+	ZetaG: {
+		HP: 10,
+		Dex: 5,
+		Str: 3,
+		Const: 2,
+		getZeta: function() {
+			return this.ZetaG
+		}
+	},
+
+	Sinanju: {
+		HP: 10,
+		Dex: 4,
+		Str: 4,
+		Const: 3, 
+		getSinanju: function() {
+			return this.Sinanju
+		}
+	},
+
+ 	CommQuantG: {
+		HP: 10,
+		Dex: 4,
+		Str: 3,
+		Const: 3,
+		getCommQuant: function() {
+			return this.getCommQuantG
+		}
+	},
+
+	ShiningG: {
+		HP: 10,
+		Dex: 2,
+		Str: 5,
+		Const: 3,
+		getShining: function() {
+			return this.ShiningG
+		}
+	},
+}
+
 function App() {
   return (
     <div className="App">
