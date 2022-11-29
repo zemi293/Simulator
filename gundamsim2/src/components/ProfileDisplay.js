4e2f20e89440b16066ca9345e423f49b7ae273c7
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-29 10:05:58 -0700

    created profile display component

--- gundamsim2/src/components/ProfileDisplay.js
+++ gundamsim2/src/components/ProfileDisplay.js
@@ -1,3 +1,11 @@
 import React from "react";
 import Profile from "./GundamProfiles/GundamProfile.js";
 
+class ProfileDisplay extends React.Component{
+    render() {
+        
+        return(
+
+        )
+    }
+}
